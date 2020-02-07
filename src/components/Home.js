c755a065cfa9e8d676f16b238d9385a027e3ee7d
HugoSanchez <hugosanchez251@gmail.com>
2020-02-08 00:20:00 +0100

    saved 3box profile into redux state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,44 @@
 import React, {useState, useEffect} from 'react';
-import {connect} from 'react-redux';
-import Box from '3box';
-import {setEthereumAddress_Action} from '../actions';
-
-import Header from './common/Header';
+import {useDispatch, useSelector} from "react-redux";
 import clipUploading from '../resources/clipUploading.png';
 import Button from './common/Button';
+import Header from './common/Header';
+import Box from '3box';
+import {
+    setEthereumAddress_Action, 
+    setUserProfile_Action
+} from '../actions';
 import '../App.css';
 
-const Home = ({props, dispatch}) => {
+const Home = () => {
 
-    const [entries, setEntries] = useState([]); 
-    const [renderModal, setRenderModal] = useState(false);
+    const dispatch = useDispatch();
+    const profile = useSelector(state => state.user.profile);
 
-    const getAddressFromMetamask = async () => {
+    // ComponentDidMount equivalent.
+    useEffect(async () => {
+        await FetchUserProfile()
+    }, [])
+    
+    // Get the user Ethereum address from Metamask.
+    const FetchUserProfile = async () => {
         if (typeof window.ethereum == 'undefined') {
             alert('Please install metamask to proceed')
         } else {
             const accounts = await window.ethereum.enable();
             const box = await Box.openBox(accounts[0], window.ethereum)
             const profile = await Box.getProfile(accounts[0])
-            const space = await box.openSpace('bradbvry-main')
-            console.log('Accounts: ', accounts)
-            console.log('profile: ', profile)
             dispatch(setEthereumAddress_Action(accounts[0]))
+            dispatch(setUserProfile_Action(profile))
         }
     }
 
-    useEffect(() => {
-        getAddressFromMetamask()
-        if (entries.length < 1) { setRenderModal(true)} else {setRenderModal(false)}
-    })
-    
     return (
         <div className="App">
             <Header />
+            {
+                console.log('Count:', profile)
+            }
             <div className="Main">
                 <div className="Modal">
                     <img src={clipUploading} id="Modal-Image" />
@@ -48,6 +52,4 @@ const Home = ({props, dispatch}) => {
     );
 }
 
-export default connect(state => ({
-    user: state.user
-}))(Home);
+export default Home;
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,23 +1,20 @@
-import { userActions } from '../actions';
-import { useReducer } from 'react';
-
 import {
   SET_ETHEREUM_ADDRESS,
   SET_USER_PROFILE
 } from '../actions/types';
 
 const initialState = {
-    user: null
+    profile: null,
+    address: null
 }
 
 const userProfileReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SET_USER':
-        console.log('Action: ', action.type)
-        console.log('Payload: ', action.payload)
-      return {user: action.payload}
-    default:
-      return state
+    case SET_USER_PROFILE:     
+      return {profile: action.payload}
+    case SET_ETHEREUM_ADDRESS: 
+      return {address: action.payload}
+    default: return state
   }
 }
 export default userProfileReducer
