7fcd73eb14efd1cd493fb55d2e532b6b533b0201
HugoSanchez <hugosanchez251@gmail.com>
2020-02-11 18:33:02 +0100

    transformed Home component to Class, testing pending

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import React, {Component} from 'react';
+import {connect} from "react-redux";
 import clipUploading from '../resources/clipUploading.png';
 import Button from './common/Button';
 import Header from './common/Header';
@@ -10,43 +10,70 @@ import {
 } from '../actions';
 import '../App.css';
 
-const Home = () => {
-
-    const dispatch = useDispatch();
-    const profile = useSelector(state => state.user.profile);
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderMetamaskModal: false,
+        }
+    }
 
     /**
      *      const spaceData = await space.private.all()
             console.log('Space Data: ', spaceData)
      */
 
-    useEffect(() => {FetchUserProfile()}, [])
-    
-    const FetchUserProfile = async () => {
-        if (typeof window.ethereum == 'undefined') {
-            alert('Please install metamask to proceed')
-        } else {
-            const accounts = await window.ethereum.enable();
-            const profile = await Box.getProfile(accounts[0])
-            dispatch(setEthereumAddress_Action(accounts[0]))
-            dispatch(setUserProfile_Action(profile))
-        }
+    async componentWillMount(){
+        await this.handleMetamask()
+        await this.fetchUserProfile()
     }
 
-    return (
-        <div className="App">
-            <Header />
-            <div className="Main">
-                <div className="Modal">
-                    <img src={clipUploading} id="Modal-Image" />
-                    <h1 id="empty-home-title">Hi there!</h1>
-                    <p id="empty-home-text">It appears that you haven't saved any entries yet.</p>
-                    <p id="empty-home-text-2"> Click on the button below to start creating new memories.</p>
-                    <Button id="empty" path={"/editor"} text="New entry" onClick={() => console.log('YEP!!')}/>
+    async handleMetamask() {
+        if (typeof window.ethereum == 'undefined') {
+            this.setState({renderMetamaskModal: true})} 
+        else {
+
+            let accounts = await window.ethereum.enable();
+            setEthereumAddress_Action(accounts[0])
+
+        }
+    }
+    
+    async fetchUserProfile() {
+        let {address} = this.state
+        let profile = await Box.getProfile(address)
+        setUserProfile_Action(profile)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <div className="Main">
+                    <div className="Modal">
+                        <img src={clipUploading} id="Modal-Image" />
+                        <h1 id="empty-home-title">Hi there!</h1>
+                        <p id="empty-home-text">It appears that you haven't saved any entries yet.</p>
+                        <p id="empty-home-text-2"> Click on the button below to start creating new memories.</p>
+                        <Button id="empty" path={"/editor"} text="New entry" onClick={() => console.log('YEP!!')}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+    profile: state.user.profile
+})
+  
+const mapDispatchToProps = dispatch => ({
+    setEthereumAddress_Action: add => dispatch(setEthereumAddress_Action(add)),
+    setUserProfile_Action: profile => dispatch(setUserProfile_Action(profile))   
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Home)
+
