bd41088db666ad5dfdef1bd86a19d1e46138bbb9
HugoSanchez <hugosanchez251@gmail.com>
2020-10-27 08:18:42 +0100

    basic functionally of redux-persist is working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react'
 import configureStore from './store';
 import LandingPage from './containers/LandingPage';
 import Settings from './containers/Settings';
@@ -14,7 +15,7 @@ import {
 import Home from './containers/Home';
 import './App.css';
 
-const store = configureStore();
+const {store, persistor} = configureStore();
 
 class App extends Component {
 
@@ -23,17 +24,19 @@ class App extends Component {
 
             <main className="App">
                 <Provider store={store}>
-                    <Switch>
-                        <Route path='/signin' component={SignIn} />
-                        <Route path='/landing' component={LandingPage} />          
-                        <Route path='/app/add-member/:memberAddress/:thread/:threadName/:email' component={AddMember}/>
-                        <Route path='/app/accept-invite/:user/:thread/:threadName' component={JoinCollection}/>
-                        <Route path='/app/:user/:threadAddress/:threadName' component={Collection} />
-                        <Route path='/app/:user' component={Home} />
-                        <Route path='/editor' component={Editor} />
-                        <Route path='/settings' component={Settings} />
-                        <Route exact path="/" render={() => (<Redirect to="/landing" />)} /> 
-                    </Switch>
+                    <PersistGate loading={null} persistor={persistor}>
+                        <Switch>
+                            <Route path='/signin' component={SignIn} />
+                            <Route path='/landing' component={LandingPage} />          
+                            <Route path='/app/add-member/:memberAddress/:thread/:threadName/:email' component={AddMember}/>
+                            <Route path='/app/accept-invite/:user/:thread/:threadName' component={JoinCollection}/>
+                            <Route path='/app/:user/:threadAddress/:threadName' component={Collection} />
+                            <Route path='/app/:user' component={Home} />
+                            <Route path='/editor' component={Editor} />
+                            <Route path='/settings' component={Settings} />
+                            <Route exact path="/" render={() => (<Redirect to="/landing" />)} /> 
+                        </Switch>
+                    </PersistGate>
                 </Provider>
             </main>
         );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
 import rootSaga from '../sagas';
 import rootReducer from '../reducers'; 
 import createSagaMiddleware from 'redux-saga';
+import storage from 'redux-persist/lib/storage';
+import * as Flatted from 'flatted'; 
+
+import { 
+    persistStore, 
+    persistReducer, 
+    createTransform 
+} from 'redux-persist'
+
 import { 
     compose,
     createStore, 
@@ -8,19 +17,33 @@ import {
 } from 'redux';
 
 
+const transformCircular = createTransform(
+    (inboundState, key) => Flatted.stringify(inboundState),
+    (outboundState, key) => Flatted.parse(outboundState),
+)
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    transforms: [transformCircular]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const configureStore = () => {
 
     const sagaMiddleware = createSagaMiddleware()
     const store = createStore(
-        rootReducer,
+        persistedReducer,
         compose(
             applyMiddleware(sagaMiddleware)
             // window.__REDUX_DEVTOOLS_EXTENSION__ &&
                 // window.__REDUX_DEVTOOLS_EXTENSION__(),
         )
     );
+    const persistor = persistStore(store)
     sagaMiddleware.run(rootSaga)
-    return store;
+    return {store, persistor};
 }
 
 export default configureStore
